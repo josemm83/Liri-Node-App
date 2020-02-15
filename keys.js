c9ce5136ce4e69d4b461f0dc41a9c236097f4b27
josemm83 <josemmancha@gmail.com>
2020-02-15 17:09:23 -0600

    updating homework files

--- keys.js
+++ keys.js
@@ -3,4 +3,4 @@ console.log('this is loaded');
 exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
-};touch
+};
--- liri.js
+++ liri.js
@@ -1,1 +1,40 @@
-var Spotify = require('node-spotify-api');
+require("dotenv").config();
+var axios = require("axios");
+var Spotify = require("node-spotify-api");
+var fs = require("fs");
+
+var keys = require("./keys");
+var spotify = new Spotify(keys.spotify);
+var choice = process.argv[2];
+var userInput = process.argv.slice(3).join(" ");
+
+switch(choice) {
+    case "concert-this":
+        break;
+    case "spotify-this-song":
+        break;
+    case "movie-this":
+        break;
+    case "do-what-it-says":
+        readMe("random.txt");
+        break;
+    default:
+        console.log("Not a valid command");
+}
+
+function findSong(){
+
+}
+
+function findMovie(){
+
+}
+
+function readMe(file){
+    fs.readFile(file, "utf8", function(error, data){
+        if(error){
+            return console.log(error);
+        }
+
+    });
+}
